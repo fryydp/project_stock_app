5560c9fda7d1efd679a23de94ab12f679f707225
fryydp <fryydp@gmail.com>
2025-02-24 02:39:09 +0000

    dashboard

--- app/Http/Controllers/dashboardController.php
+++ app/Http/Controllers/dashboardController.php
@@ -3,11 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\stok;
+use App\Models\suplier;
 
 class dashboardController extends Controller
 {
     public function index()
     {
-    return view('Dashboard.dashboard');
+        $getSuplier = suplier::count();
+        $getPelanggan = pelanggan::count();
+        $getStok = stok::count();
+        $getPendapatan = barangKeluar::sum('sub_total');
+        return view('Dashboard.dashboard', compact(
+            'getSuplier',
+            'getPelanggan',
+            'getStok',
+            'getPendapatan',
+        ));
     }
 }
